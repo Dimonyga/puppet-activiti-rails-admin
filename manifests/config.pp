0c07750968e94f20eceb8bccfbe560e42966ece6
Dimonyga <Dimonyga@gmail.com>
2015-11-19 13:43:04 +0600

    fix

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,11 @@
 class activiti_rails_admin::config inherits activiti_rails_admin {
 
-  $hiera_servers = hiera_hash (activiti_rails_admin::activiti_admin_servers)
-  $servers = hash_merge($hiera_servers,$activiti_rails_admin::params::activiti_admin_servers)
+  if any2bool (activiti_rails_admin::activiti_admin_servers) {
+    $hiera_servers = hiera_hash (activiti_rails_admin::activiti_admin_servers)
+    $servers = hash_merge($hiera_servers,$activiti_rails_admin::params::activiti_admin_servers)
+  } else {
+    $servers = $activiti_rails_admin::params::activiti_admin_servers
+  }
 
   file { "${activiti_admin_home}/config/servers.yml":
     ensure  => file,
